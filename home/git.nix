1b8ca27d89ea3f075a529ad4d5561657e8ef1985
Jake Turner <jaketurner624@gmail.com>
2026-07-04 19:54:07 -0400

    Added: ssh

--- home/git.nix
+++ home/git.nix
@@ -8,4 +8,13 @@
       email = "jaketurner624@gmail.com";
     };
   };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks."github.com" = {
+      hostname     = "github.com";
+      user         = "git";
+      identityFile = "~/.ssh/id_ed25519";
+    };
+  };
 }
