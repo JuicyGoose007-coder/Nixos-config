afe1a9ea15e5dbe85d2af5a95f04aa73b04abe1f
Jake Turner <jaketurner624@gmail.com>
2026-08-08 11:14:39 -0400

    fix(home/iris.nix): update vendorHash for iris package

--- home/iris.nix
+++ home/iris.nix
@@ -38,7 +38,7 @@ let
     subPackages = [ "cmd/iris" ];
 
     proxyVendor = true;
-    vendorHash = "sha256-KQNloP/Aj283YQ4d5LFu/2Pbb2HbVTZPhLK1fs4xvGw=";
+    vendorHash = "sha256-q1szUQkhdKq2VhMuWYYWTahmDxGeVjvHLmjciZu3cBU=";
 
     doCheck = false;
 
